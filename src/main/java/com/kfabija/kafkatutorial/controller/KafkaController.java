42a48782545d7d58c026a4bb0d00f4f5683dff9a
KarloFab <fabijanickarlo1@gmail.com>
2021-02-02 20:03:00 +0100

    Added kafka docker compose file

--- src/main/java/com/kfabija/kafkatutorial/controller/KafkaController.java
+++ src/main/java/com/kfabija/kafkatutorial/controller/KafkaController.java
@@ -2,10 +2,7 @@ package com.kfabija.kafkatutorial.controller;
 
 import com.kfabija.kafkatutorial.service.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/kafka")
