758913aeb644582c32d2da3c4791281318293902
KarloFab <fabijanickarlo1@gmail.com>
2021-02-02 18:09:17 +0100

    initial commit

---
+++ src/test/java/com/kfabija/kafkatutorial/KafkatutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfabija.kafkatutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkatutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
